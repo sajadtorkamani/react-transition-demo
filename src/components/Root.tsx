27342db194a2bf2fb7bbfc959dc24c429d3e4439
Sajad Torkamani <sajadtorkamani1@gmail.com>
2024-11-10 13:21:59 +0000

    wip

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 
+import Footer from './Footer'
 import Header from './Header'
 
 const Root: React.FC = () => (
   <>
     <Header />
-    <Outlet />
+    <div className="main-content">
+      <Outlet />
+    </div>
+    <Footer />
   </>
 )
 
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,3 +4,6 @@ export const ROUTES = {
   withoutTransitionExample: '/without-transition-example',
   withTransitionExample: '/with-transition-example',
 }
+
+export const GITHUB_REPO_URL =
+  'https://github.com/sajadtorkamani/react-transition-demo'
